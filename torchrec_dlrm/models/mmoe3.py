d280de3bae49a02fa6ab7ee75ef87bea8e5a421e
zhouyu5 <yu.zhou@intel.com>
2023-06-19 18:14:42 -0400

    mmoe3

--- torchrec_dlrm/models/mmoe3.py
+++ torchrec_dlrm/models/mmoe3.py
@@ -3,8 +3,6 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import torch.utils.data as Data
-from torch.utils.data import DataLoader
 
 from deepctr_torch.models.basemodel import BaseModel
 from deepctr_torch.layers import DNN, PredictionLayer
@@ -40,24 +38,78 @@ class MMOE3(MMOE2):
 
     def __init__(self, dnn_feature_columns, num_experts=3, expert_dnn_hidden_units=(256, 128),
                  gate_dnn_hidden_units=(64,), tower_dnn_hidden_units=(64,), l2_reg_linear=0.00001,
-                 l2_reg_embedding=0.00001, l2_reg_dnn=0,
+                 l2_reg_embedding=0.00001, l2_reg_dnn=0, num_classes=4,
                  init_std=0.0001, seed=1024, dnn_dropout=0, dnn_activation='relu', dnn_use_bn=False,
                  task_types=('binary', 'binary'), task_names=('ctr', 'ctcvr'), device='cpu', gpus=None):
-        super(MMOE3, self).__init__(dnn_feature_columns, num_experts, expert_dnn_hidden_units,
-                 gate_dnn_hidden_units, tower_dnn_hidden_units, l2_reg_linear,
-                 l2_reg_embedding, l2_reg_dnn,
-                 init_std, seed, dnn_dropout, dnn_activation, dnn_use_bn,
-                 task_types, task_names, device, gpus)
-        
+        super(MMOE2, self).__init__(linear_feature_columns=[], dnn_feature_columns=dnn_feature_columns,
+                                   l2_reg_linear=l2_reg_linear, l2_reg_embedding=l2_reg_embedding, init_std=init_std,
+                                   seed=seed, device=device, gpus=gpus)
+        self.num_tasks = len(task_names)
+
+        assert len(tower_dnn_hidden_units) > 0, "tower_dnn_hidden_units "\
+            "size must > 0"
+
+        if self.num_tasks <= 1:
+            raise ValueError("num_tasks must be greater than 1")
+        if num_experts <= 1:
+            raise ValueError("num_experts must be greater than 1")
+        if len(dnn_feature_columns) == 0:
+            raise ValueError("dnn_feature_columns is null!")
+        if len(task_types) != self.num_tasks:
+            raise ValueError("num_tasks must be equal to the length of task_types")
+
+        for task_type in task_types:
+            if task_type not in ['binary', 'regression', 'multiclass']:
+                raise ValueError("task must be binary or multiclass or regression, {} is illegal".format(task_type))
+
+        self.num_experts = num_experts
+        self.task_names = task_names
+        self.input_dim = self.compute_input_dim(dnn_feature_columns)
+        self.expert_dnn_hidden_units = expert_dnn_hidden_units
+        self.gate_dnn_hidden_units = gate_dnn_hidden_units
+
+        # expert dnn
+        self.expert_dnn = nn.ModuleList([DNN(self.input_dim, expert_dnn_hidden_units, activation=dnn_activation,
+                                             l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout, use_bn=dnn_use_bn,
+                                             init_std=init_std, device=device) for _ in range(self.num_experts)])
+
+        # gate dnn
+        if len(gate_dnn_hidden_units) > 0:
+            self.gate_dnn = nn.ModuleList([DNN(self.input_dim, gate_dnn_hidden_units, activation=dnn_activation,
+                                               l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout, use_bn=dnn_use_bn,
+                                               init_std=init_std, device=device) for _ in range(self.num_tasks)])
+            self.add_regularization_weight(
+                filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], self.gate_dnn.named_parameters()),
+                l2=l2_reg_dnn)
+        self.gate_dnn_final_layer = nn.ModuleList(
+            [nn.Linear(gate_dnn_hidden_units[-1] if len(gate_dnn_hidden_units) > 0 else self.input_dim,
+                       self.num_experts, bias=False) for _ in range(self.num_tasks)])
+
         # tower dnn (task-specific)
         self.tower_dnn = nn.ModuleList(
+            [DNN(expert_dnn_hidden_units[-1], 
+                    tower_dnn_hidden_units, activation=dnn_activation,
+                    l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout, use_bn=dnn_use_bn,
+                    init_std=init_std, device=device)] + \
             [DNN(expert_dnn_hidden_units[-1] + tower_dnn_hidden_units[-1], 
                     tower_dnn_hidden_units, activation=dnn_activation,
                     l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout, use_bn=dnn_use_bn,
-                    init_std=init_std, device=device) for _ in range(self.num_tasks)]
+                    init_std=init_std, device=device) for _ in range(self.num_tasks-1)]
         )
         self.add_regularization_weight(
             filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], self.tower_dnn.named_parameters()),
             l2=l2_reg_dnn)
-        
+
+        self.tower_dnn_final_layer = nn.ModuleList(
+            [nn.Linear(tower_dnn_hidden_units[-1], num_classes, bias=False)] + \
+            [nn.Linear(tower_dnn_hidden_units[-1], 1, bias=False)
+                for _ in range(self.num_tasks-1)]
+        )
+
+        self.out = nn.ModuleList([PredictionLayer(task) for task in task_types])
+
+        regularization_modules = [self.expert_dnn, self.gate_dnn_final_layer, self.tower_dnn_final_layer]
+        for module in regularization_modules:
+            self.add_regularization_weight(
+                filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], module.named_parameters()), l2=l2_reg_dnn)
         self.to(device)
